02300af0cafc763d1c5fad5319e88b13b01d1fea
FloSewn <florian.setzwein@gmx.de>
2022-10-23 21:42:31 +0200

    Remove windows from test chain and restore original compilation script

--- scripts/compile_tqmesh.sh
+++ scripts/compile_tqmesh.sh
@@ -15,9 +15,7 @@ mkdir -p build
 cd build
 
 cmake .. -DCMAKE_CXX_COMPILER=${COMPILER} -DCMAKE_BUILD_TYPE=${BUILDTYPE}
-#make install -j${CORES}
-
-mingw32-make install -j${CORES}
+make install -j${CORES}
 
 cd ..
 
