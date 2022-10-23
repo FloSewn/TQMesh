566be8a164d90adc35346ff9afcb4d5d2f4e6fee
FloSewn <florian.setzwein@gmx.de>
2022-10-23 21:26:21 +0200

    Comment execution of examples from testing scripts

--- scripts/test_tqmesh.sh
+++ scripts/test_tqmesh.sh
@@ -5,17 +5,17 @@ set -e
 cd build
 ctest
 
-./../bin/TQMesh ../input/Example_1.para
-./../bin/TQMesh ../input/Example_2.para
-./../bin/TQMesh ../input/Example_3.para
-./../bin/TQMesh ../input/Example_4.para
-./../bin/TQMesh ../input/Example_5.para
+#./../bin/TQMesh ../input/Example_1.para
+#./../bin/TQMesh ../input/Example_2.para
+#./../bin/TQMesh ../input/Example_3.para
+#./../bin/TQMesh ../input/Example_4.para
+#./../bin/TQMesh ../input/Example_5.para
 
-./../bin/run_examples 1
-./../bin/run_examples 2
-./../bin/run_examples 3
-./../bin/run_examples 4
-./../bin/run_examples 5
+#./../bin/run_examples 1
+#./../bin/run_examples 2
+#./../bin/run_examples 3
+#./../bin/run_examples 4
+#./../bin/run_examples 5
 
 cd ..
 
